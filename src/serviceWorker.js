3cb8018ee1ff2fbc21b941184b15692f30afb20e
Yogesh <yogesh@yarddigital.com>
2021-04-11 20:29:58 +0100

    hostname updates

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -9,15 +9,9 @@
 
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
-
+// [::1] is the IPv6 localhost address. // 127.0.0.0/8 are considered localhost for IPv4.
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  window.location.hostname === 'firechat-application.netlify.app' || window.location.hostname === '[::1]' || window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
